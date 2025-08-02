c11d97f35401da150f5f10e54ffc568f97a9e555
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-08-02 17:58:49 -0400

    proper routes set up

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AddCreator from './pages/AddCreator'
 import EditCreator from './pages/EditCreator';
 import ShowCreators from './pages/ShowCreators';
 import ViewCreator from './pages/ViewCreator';
+import FOUROFOUR from './pages/FOUROFOUR';
 
 function App() {
   return (
@@ -15,13 +16,14 @@ function App() {
       <h1 style={{ color: 'red' }}>This is app.js</h1>
     </div>
       <Routes>
-        <Route element={AddCreator} path='/add-creator'/>
-        <Route element={EditCreator} path='/edit-creator'/>
-        <Route element={ShowCreators} path='/show-creators'/>
-        <Route element={ViewCreator} path='/view-creator'/>
+        <Route element={<AddCreator />} path='/add-creator'/>
+        <Route element={<EditCreator />} path='/edit-creator'/>
+        <Route element={<ShowCreators />} path='/show-creators'/>
+        <Route element={<ViewCreator />} path='/view-creator'/>
+        <Route element={<FOUROFOUR />} path='/*'/>
       </Routes>
     </BrowserRouter>
-  
+
   );
 }
 
