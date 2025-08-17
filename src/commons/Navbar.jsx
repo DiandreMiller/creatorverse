cef4be5143599e96cac2cb70aaf3a27bef465467
Diandre Miller <diandremiller@diandres-air.mynetworksettings.com>
2025-08-17 04:31:21 -0400

    cleanup

--- src/commons/Navbar.jsx
+++ src/commons/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
 import "@picocss/pico/css/pico.min.css";
-import "../components/cosmic.css"; // keep consistent cosmic vibe
+import "../components/cosmic.css"; 
 
 const Navbar = () => {
   const location = useLocation();
